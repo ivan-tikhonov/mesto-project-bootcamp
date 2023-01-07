6630e7f29f1eb23a1b26f018e2ffae79dd9fb76c
Ivan Tikhonov <ivantikhonov1989@gmai.com>
2023-01-07 11:06:48 +0300

    feat: добавить валидацию формы профайла

--- scripts/script.js
+++ scripts/script.js
@@ -135,7 +135,13 @@ profileEditButton.addEventListener('click', function(e) {
   const profileOccupationValue = profileOccupationElement.textContent;
   popupProfileName.value = profileNameValue;
   popupProfileOccupation.value = profileOccupationValue;
-
+  enableValidation({
+    formSelector: formProfile,
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button-save',
+    inputErrorClass: 'popup__input_state_invalid',
+    errorClass: 'popup__input-error_active'
+  });
 });
 
 elementEditButton.addEventListener('click', function(e) {
@@ -149,4 +155,66 @@ popupImageCloseButton.addEventListener('click', hideClosestPopup);
 
 formProfile.addEventListener('submit', handleProfileFormSubmit);
 cardForm.addEventListener('submit', handleElementFormSubmit);
+
+
+
+
+
+
+
+
+function showInputError(configObject, formElement, inputElement,errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(configObject.inputErrorClass);
+  errorElement.classList.add(configObject.errorClass);
+  errorElement.textContent = errorMessage;
+}
+
+function hideInputError(configObject, formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(configObject.inputErrorClass);
+  errorElement.classList.remove(configObject.errorClass);
+}
+
+function isValid(configObject, formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(configObject, formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(configObject, formElement, inputElement);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    console.log(inputElement);
+    console.log(inputElement.validity);
+    return !inputElement.validity.valid;
+  });
+};
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.disabled = false;
+  }
+}
+
+function setEventListener(configObject, formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(configObject.inputSelector));
+  const buttonElement = formElement.querySelector(configObject.submitButtonSelector);
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(configObject, formElement, inputElement)
+      toggleButtonState(inputList, buttonElement);
+    })
+  });
+};
+
+function enableValidation(configObject) {
+  const form = configObject.formSelector;
+  setEventListener(configObject, form);
+};
+
 
